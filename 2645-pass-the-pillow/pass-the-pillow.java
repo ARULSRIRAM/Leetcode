7bf9167083454438cb68afb306cb118033e56ea8
ARUL M <arulsriram2006@gmail.com>
2026-02-10 12:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (15.32%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int full=time/(n-1);
+        int extra=time%(n-1);
+        if(full%2==0)return extra+1;
+        return n-extra;
+    }
+}
