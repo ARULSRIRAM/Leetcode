b6018667b00876fafe05f1b12f506663c92513f6
ARUL M <arulsriram2006@gmail.com>
2026-02-19 15:18:15 +0530

    Time: 1 ms (87.96%) | Memory: 44.5 MB (19.71%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,29 @@
 class Solution {
+    private static List<List<Integer>> res;
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        int len=1<<nums.length;
-        for(int i=0;i<len;i++){
-            List<Integer> res=new ArrayList<>();
-            for(int j=0;j<nums.length;j++){
-                if((i &1<<j)!=0){
-                    res.add(nums[j]);
-                }
-            }
-            ans.add(res);
+        // Using Bit manuplation
+        // List<List<Integer>> ans=new ArrayList<>();
+        // int len=1<<nums.length;
+        // for(int i=0;i<len;i++){
+        //     List<Integer> res=new ArrayList<>();
+        //     for(int j=0;j<nums.length;j++){
+        //         if((i &1<<j)!=0){
+        //             res.add(nums[j]);
+        //         }
+        //     }
+        //     ans.add(res);
+        // }
+        // return ans;
+        res=new ArrayList<>();
+        backtrack(0,new ArrayList<>(),nums);
+        return res;
+    }
+    private void backtrack(int index,List<Integer> temp,int[] nums){
+        res.add(new ArrayList<>(temp));
+        for(int i=index;i<nums.length;i++){
+            temp.add(nums[i]);
+            backtrack(i+1,temp,nums);
+            temp.remove(temp.size()-1);
         }
-        return ans;
     }
 }
