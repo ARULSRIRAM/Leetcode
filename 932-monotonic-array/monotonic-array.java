487a0eed226d4ac2f9a5de3bc73f629e8f9f3045
ARUL M <arulsriram2006@gmail.com>
2026-04-17 19:48:33 +0530

    Time: 2 ms (67.34%) | Memory: 84.6 MB (98.24%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -3,15 +3,13 @@ class Solution {
         boolean asc=true;
         boolean desc=true;
         for(int i=0;i<nums.length-1;i++){
-            if(nums[i]<nums[i+1]){
+            if(!(nums[i]<=nums[i+1])){
                 asc=false;
             }
-             if(nums[i]>nums[i+1]){
+            if(!(nums[i]>=nums[i+1])){
                 desc=false;
             }
-            if(!asc && !desc)return false;
         }
-
-        return asc ? asc:desc; 
+        return (asc || desc)?true:false;
     }
 }
