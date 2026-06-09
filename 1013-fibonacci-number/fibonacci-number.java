3d9fefab2175e218f3608acfa70a7d059e14ccce
ARUL M <arulsriram2006@gmail.com>
2026-06-09 14:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (32.59%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,21 @@
 class Solution {
     public int fib(int n) {
-        int[] arr=new int[n+1];
-        Arrays.fill(arr,-1);
-        return fibonacci(arr,n);
-    }
-    public int fibonacci(int dp[],int n){
         if(n==0 || n==1)return n;
-        if(dp[n]!=-1)return dp[n];
+        int[] arr=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+        
+    }
 
-        return dp[n]=fibonacci(dp,n-1)+fibonacci(dp,n-2);
-    } 
+    //Memoization
+    // public int fibonacci(int dp[],int n){
+    //     if(n==0 || n==1)return n;
+    //     if(dp[n]!=-1)return dp[n];
+
+    //     return dp[n]=fibonacci(dp,n-1)+fibonacci(dp,n-2);
+    // } 
 }
