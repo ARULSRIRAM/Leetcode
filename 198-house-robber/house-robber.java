ad61840de6dcdc4be1f9d6ecbbb584d5e5169dfa
ARUL M <arulsriram2006@gmail.com>
2026-07-10 15:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (18.45%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,37 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
         int[] dp=new int[nums.length];
-        // Arrays.fill(dp,-1);
-        // return house(nums.length-1,nums,dp);
-
-
-
-        //Tabulation
-        if(nums.length==1)return nums[0];
+         if(nums.length==1)return nums[0];
+        return rob(nums.length-1,nums,dp);
+    }
+    int rob(int n,int[]nums,int[] dp){
+    
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length;i++){
-            int steal=nums[i]+dp[i-2];
-            int notSteal=dp[i-1];
-            dp[i]=Math.max(steal,notSteal);
+        for(int j=2;j<=n;j++){
+            dp[j]=Math.max(dp[j-2]+nums[j],dp[j-1]);
         }
-        return dp[nums.length-1];
-
-
-
+        return dp[n];
     }
-    // public int house(int n,int[] nums){
-    //     if(n<0)return 0;
-    //     int steal=nums[n]+house(n-2,nums);
-    //     int notsteal=house(n-1,nums);
-    //     return Math.max(steal,notsteal);
-    // }
-     public int house(int n,int[] nums,int[] dp){
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        int steal=nums[n]+house(n-2,nums,dp);
-        int notsteal=house(n-1,nums,dp);
-        return dp[n]=Math.max(steal,notsteal);
-    }
-    
 }
