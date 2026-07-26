7525ba4bc5f443ee621fd303f05967fc26bb350b
ARUL M <arulsriram2006@gmail.com>
2026-07-26 22:28:16 +0530

    Time: 5 ms (81.58%) | Memory: 46.5 MB (99.88%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,9 +1,34 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n=nums.length;
-        int case1=nums[n-1]*nums[n-2]*nums[n-3];
-        int case2=nums[0]*nums[1]*nums[n-1];
-        return Math.max(case1,case2);
+        int fmax=Integer.MIN_VALUE;
+        int smax=Integer.MIN_VALUE;
+        int tmax=Integer.MIN_VALUE;
+        int fmin=Integer.MAX_VALUE;
+        int smin=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>fmax){
+                tmax=smax;
+                smax=fmax;
+                fmax=nums[i];
+            }
+            else if(fmax>=nums[i] && smax<nums[i]){
+                tmax=smax;
+                smax=nums[i];
+            }
+            else if(fmax>=nums[i] && smax>=nums[i] && nums[i] >tmax){
+                tmax=nums[i];
+            }
+            if(nums[i]<fmin){
+                smin=fmin;
+                fmin=nums[i];
+            }
+            else if(fmin<=nums[i] && smin>nums[i]){
+                smin=nums[i];
+            }
+        }
+        int val1=fmax*smax*tmax;
+        int val2=fmax*fmin*smin;
+        return (val1>val2)?val1:val2;
+
     }
 }
