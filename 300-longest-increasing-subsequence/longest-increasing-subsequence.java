1729ae30a613b3267ca0f6e932792546932fceff
ARUL M <arulsriram2006@gmail.com>
2026-08-12 14:33:17 +0530

    Time: 37 ms (73.21%) | Memory: 46.2 MB (43.84%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,25 +1,44 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[][] dp=new int[n][n-1];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
-        }
-        return LIS(0,-1,nums,dp);
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        // dp[i] - Length of LIS till index i
+        int ans=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            ans=Math.max(ans,dp[i]);
+        }     
+        return ans;
     }
-    public static int LIS(int i,int prev,int[] nums,int[][] dp){
-        if(i == nums.length)return 0;
+
+
+    //Memoization
+    // public int lengthOfLIS(int[] nums) {
+    //     int n=nums.length;
+    //     int[][] dp=new int[n][n-1];
+    //     for(int[] rows:dp){
+    //         Arrays.fill(rows,-1);
+    //     }
+    //     return LIS(0,-1,nums,dp);
+    // }
+    // public static int LIS(int i,int prev,int[] nums,int[][] dp){
+    //     if(i == nums.length)return 0;
         
-        if(prev != -1 && dp[i][prev]!=-1){
-            return dp[i][prev];
-        }
-        int pick=0;
-        if(prev == -1 || nums[i]>nums[prev]){
-            pick=1+LIS(i+1,i,nums,dp);
-        }
-        int notpick=LIS(i+1,prev,nums,dp);
-        if(prev!=-1)
-        return dp[i][prev]=Math.max(pick,notpick);
-        else return Math.max(pick,notpick);
-    }
+    //     if(prev != -1 && dp[i][prev]!=-1){
+    //         return dp[i][prev];
+    //     }
+    //     int pick=0;
+    //     if(prev == -1 || nums[i]>nums[prev]){
+    //         pick=1+LIS(i+1,i,nums,dp);
+    //     }
+    //     int notpick=LIS(i+1,prev,nums,dp);
+    //     if(prev!=-1)
+    //     return dp[i][prev]=Math.max(pick,notpick);
+    //     else return Math.max(pick,notpick);
+    // }
 }
